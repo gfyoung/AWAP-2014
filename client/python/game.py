0eb6ff482ef9b56b3c634175aa7f6f205d9fda2c
gfyoung <gfyoung@mit.edu>
2014-10-25 12:55:29 -0400

    TROLL LOL Test 2

--- client/python/game.py
+++ client/python/game.py
@@ -3,7 +3,7 @@
 # Moves via stdout in the form of "# # # #" (block index, # rotations, x, y) #
 # Important function is find_move, which should contain the main AI          #
 ##############################################################################
-print "TROLL LOL LOL LOL"
+print "May the Trolling Begin!"
 
 import sys
 import json
