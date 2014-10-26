4dc854dc61560f76b34295f884ac7155d8d28f39
gfyoung <gfyoung@mit.edu>
2014-10-25 23:25:37 -0400

    Assertion Test XIII

--- client/python/game.py
+++ client/python/game.py
@@ -251,7 +251,8 @@ class Game:
 
         for x in xrange(len(self.all_blocks)):
             for y, block in enumerate(self.all_blocks[x]):
-                assert not isinstance(self.all_blocks[x][y][0], tuple)
+                self.all_blocks[x][y][0] * 2
+                assert not isinstance(self.all_blocks[x][y][0], dict)
                 assert isinstance(self.all_blocks[x][y][0][0], int)
                 
                 self.all_blocks[x][y] = [Point(offset['x'], offset['y']) for offset in block]
